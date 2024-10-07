5c9cdc09e04e53e9f1725fde9dd153cbfff6fe22
Áron Demeter <demeter.aron0614@gmail.com>
2024-10-07 21:31:11 +0200

    WIP stuff

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -3,4 +3,4 @@
 pub const BASE_URL: &str = "https://rackhost.hu";
 pub const LOGIN_PATH: &str = "/site/login";
 pub const DNS_ZONE_PATH: &str = "/dnsZone";
-
+pub const DOMAIN_SEARCH_PATH: &str = "/domain"; // followed by "?domainList=<name>"
