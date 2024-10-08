422cd36ae4747784f99c988ad349975d315a98a2
Áron Demeter <demeter.aron0614@gmail.com>
2024-10-08 10:26:47 +0200

    WIP stuff

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@
 
 use std::marker::PhantomData;
 use reqwest::{Client, Error, Url};
-use scraper::Html;
+use scraper::{CaseSensitivity, Element, Html};
+use scraper::selector::CssLocalName;
 use crate::endpoints::{BASE_URL, DOMAIN_SEARCH_PATH};
 
 mod endpoints;
-
 
 struct Authed;
 struct NotAuthed;
@@ -60,12 +60,9 @@ impl RackhostClient<Authed> {
 }
 
 impl<L> RackhostClient<L> { // shared behaviour
-    
-    
     pub async fn search_domain(&self, name: impl Into<String>) -> anyhow::Result<Vec<DomainInfo>> {
-        unimplemented!();
         let url = Url::parse_with_params(
-            &format!("{}{}", BASE_URL, DOMAIN_SEARCH_PATH), 
+            &format!("{}{}", BASE_URL, DOMAIN_SEARCH_PATH),
             &[("domainList", name.into())])
             .expect("Failed to parse URL");
         
@@ -73,42 +70,60 @@ impl<L> RackhostClient<L> { // shared behaviour
         
         let body = response.text().await?;
         let doc = Html::parse_document(&body);
-        
+
         let mut domains: Vec<DomainInfo> = vec![];
-        
-        let domain_hit_selector = scraper::Selector::parse("form[data-domain-search-res]").unwrap();
+
+        // classnames
+        let class_taken = CssLocalName::from("domain-taken");
+        let class_free = CssLocalName::from("domain-free");
+
+        // NOTES: data-domain attr always holds the domain.
+        // if a domain is taken, it has a domain-taken class
+        // if not, it has a domain-free class.
+        // Domains owned by the user have a div instead of a form and just hold the data-domain attr
+
+        let domain_hit_selector = scraper::Selector::parse("form[data-domain-search-res] div.domain-hit").unwrap(); // the second part skips the form.
         let domain_owned_selector = scraper::Selector::parse("div.domain-hit[data-domain]").unwrap();
+
+        // the following selectors are ment to be used after the hit selector
+        let domain_net_price_selector = scraper::Selector::parse("span[data-behavior=netPrice]").unwrap();
+        let domain_gross_price_selector = scraper::Selector::parse("span[data-behavior=grossPrice]").unwrap();
+
         let domains_hit = doc.select(&domain_hit_selector);
-        //domains_hit.next().unwrap().has
-        
-        
-        
-        let domain_search_name_selector = scraper::Selector::parse("span.search-words").unwrap();
-        let mut search = doc.select(&domain_search_name_selector);
-        let name = search.next()
-            .unwrap()
-            .text()
-            .next()
-            .unwrap();
-        
-        // found (same as name, TODO: need to validate that the matches are the same. )
-        
-        println!("{}", name);
-        
-        let domain_ext_selector = scraper::Selector::parse("div.domain-hit span.tld").unwrap();
-        let extensions = doc.select(&domain_ext_selector);
-        for ext in extensions {
-            println!("{}", ext.text().next().unwrap());
+        for element in domains_hit {
+            let domain_name = element.attr("data-domain").unwrap();
+            let mut domain_state = DomainState::Unknown;
+
+            if element.has_class(&class_taken, CaseSensitivity::CaseSensitive) {
+                domain_state = DomainState::Unavailable
+            } else if element.has_class(&class_free, CaseSensitivity::CaseSensitive) {
+                domain_state = DomainState::Available
+            } else {
+                // idk
+                dbg!(&element);
+            }
+
+            let net_price = element.select(&domain_net_price_selector)
+                .next()
+                .expect("No net_price found")
+                .text()
+                .next()
+                .expect("text element not found");
+
+            let gross_price = element.select(&domain_gross_price_selector)
+                .next()
+                .expect("No net_price found")
+                .text()
+                .next()
+                .expect("text element not found");
+
+            let mut domain_info = DomainInfo::new(domain_name.to_owned(), domain_state);
+
+
+            println!("{}: {:?}", domain_info.url, domain_info.domain_state);
+            println!("{} ; {}", net_price, gross_price);
         }
-        
-        // get availability
-        
-        let domain_availability_selector = scraper::Selector::parse("div.domain-hit div.avail").unwrap();
-        let avails = doc.select(&domain_availability_selector);
-        for avail_txt in avails {
-            println!("{}", avail_txt.text().next().unwrap())
-        }
-        
+
         Ok(vec![])
     }
 
@@ -133,16 +148,32 @@ impl Default for RackhostClient<NotAuthed> {
 }
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
-enum DomainState {
+pub enum DomainState {
     Available,
     Unavailable,
-    OwnedByUser
+    OwnedByUser,
+    Unknown // idk how this might happen but still, its better than crashing
 }
 
 #[derive(Debug, Clone)]
-struct DomainInfo {
-    pub url: Url,
-    pub domain_state: DomainState
+pub struct DomainInfo {
+    pub url: String,
+    pub domain_state: DomainState,
+    pub net_price: Option<f64>,
+    pub gross_price: Option<f64>,
+}
+
+impl DomainInfo {
+    pub fn new(url: String, domain_state: DomainState) -> Self {
+        Self {
+            url, domain_state,
+            net_price: None, gross_price: None
+        }
+    }
+    pub fn with_price(&mut self, net_price: f64, gross_price: f64) {
+        self.net_price = Some(net_price);
+        self.gross_price = Some(gross_price);
+    }
 }
 
 #[cfg(test)]
@@ -170,7 +201,7 @@ mod tests {
     #[tokio::test]
     async fn test_domains() {
         let client = RackhostClient::default();
-        //client.search_domain("testdomain").await.unwrap();
-        //client.search_domain("othertestdomain").await.unwrap();
+        client.search_domain("testdomain").await.unwrap();
+        client.search_domain("othertestdomain").await.unwrap();
     }
 }
