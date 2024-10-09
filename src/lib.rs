fce337f9d474855448b4fc6ed5bedffa2251dd13
Zoli06 <zolixvagyok@gmail.com>
2024-10-09 23:10:47 +0200

    Added zone and record listing

--- src/lib.rs
+++ src/lib.rs
@@ -2,30 +2,48 @@ pub mod rackhost_client;
 
 #[cfg(test)]
 mod tests {
+    use crate::rackhost_client;
     use crate::rackhost_client::RackhostClient;
+
+    async fn get_client() -> RackhostClient<rackhost_client::Authed> {
+        let username = env!("RACKHOST_USERNAME");
+        let password = env!("RACKHOST_PASSWORD");
+        //let client = RackhostClient::default().authenticate(username, password).await.unwrap();
+        let client = RackhostClient::default()
+            .authenticate(username, password)
+            .await
+            .expect("Failed to authenticate");
+        client
+    }
 
     #[tokio::test]
     async fn test_login() {
-        let username = option_env!("TEST_USERNAME").expect("No username given for test");
-        let password = option_env!("TEST_PASSWORD").expect("No password given for test");
-        let rackhost_client = RackhostClient::default().authenticate(username, password).await;
-        let cli = match rackhost_client {
-            Ok(client) => client,
-            Err(err) => {
-                dbg!(&err);
-                assert!(false);
-                return;
-            }
-        };
-        
-        assert!(true)
-        
+        let _client = get_client().await;
     }
-    
+
     #[tokio::test]
     async fn test_domains() {
         let client = RackhostClient::default();
         //client.search_domain("testdomain").await.unwrap();
         //client.search_domain("othertestdomain").await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn test_dns_zones() {
+        let client = get_client().await;
+        let zones = client.get_dns_zones().await.unwrap();
+        for zone in zones {
+            println!("{:?}", zone);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_dns_records() {
+        let client = get_client().await;
+        let zones = client.get_dns_zones().await.unwrap();
+        let records = client.get_dns_records(&zones[0]).await.unwrap();
+        for record in records {
+            println!("{:?}", record);
+        }
     }
 }
